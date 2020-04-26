765a0508b53084a03c229e961a1decccea2ad04c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-26 00:26:41 -0700

    first ios build

--- main.go
+++ main.go
@@ -33,7 +33,6 @@ func main() {
 		fmt.Fprint(os.Stderr, "TODO: usage")
 	}
 	flag.Parse()
-	fmt.Printf("iosHeader: %s\n", *iosHeader)
 	if err := mainErr(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(1)
@@ -65,12 +64,11 @@ func mainErr() error {
 		version: *version,
 		minSDK:  *minSDK,
 	}
-
-	frameworkRoot := *destPath
-	if frameworkRoot == "" {
-		frameworkRoot = fmt.Sprintf("%s.framework", strings.Title(bi.name))
+	if bi.appID == "" {
+		bi.appID = appIDFromPackage(pkgImportPath)
 	}
-	return BuildIOSFramework("tmp", frameworkRoot, bi)
+
+	return Build(bi)
 }
 
 //
@@ -87,6 +85,109 @@ type BuildInfo struct {
 	dir     string
 	archs   []string
 	minSDK  int
+}
+
+func Build(bi *BuildInfo) error {
+	tmpDir, err := ioutil.TempDir("", "gogio-")
+	if err != nil {
+		return err
+	}
+	if *keepWorkdir {
+		fmt.Fprintf(os.Stderr, "WORKDIR=%s\n", tmpDir)
+	} else {
+		defer os.RemoveAll(tmpDir)
+	}
+
+	if *archNames != "" {
+		bi.archs = strings.Split(*archNames, ",")
+	} else {
+		switch *target {
+		case "js":
+			bi.archs = []string{"wasm"}
+		case "ios", "tvos":
+			// Only 64-bit support.
+			bi.archs = []string{"arm64", "amd64"}
+		case "android":
+			bi.archs = []string{"arm", "arm64", "386", "amd64"}
+		}
+	}
+	// TODO: Bring this back?
+	//if appArgs := flag.Args()[1:]; len(appArgs) > 0 {
+	//    // Pass along arguments to the app.
+	//    bi.ldflags = fmt.Sprintf("-X gioui.org/app.extraArgs=%s", strings.Join(appArgs, "|"))
+	//}
+
+	switch *target {
+	case "js":
+		panic("-target js unimplemented")
+	case "ios", "tvos":
+		return BuildIOSFramework(tmpDir, bi)
+	case "android":
+		panic("-target android unimplemented")
+	default:
+		panic("-target required")
+	}
+}
+
+type arch struct {
+	iosArch   string
+	jniArch   string
+	clangArch string
+}
+
+var allArchs = map[string]arch{
+	"arm": {
+		iosArch:   "armv7",
+		jniArch:   "armeabi-v7a",
+		clangArch: "armv7a-linux-androideabi",
+	},
+	"arm64": {
+		iosArch:   "arm64",
+		jniArch:   "arm64-v8a",
+		clangArch: "aarch64-linux-android",
+	},
+	"386": {
+		iosArch:   "i386",
+		jniArch:   "x86",
+		clangArch: "i686-linux-android",
+	},
+	"amd64": {
+		iosArch:   "x86_64",
+		jniArch:   "x86_64",
+		clangArch: "x86_64-linux-android",
+	},
+}
+
+func appIDFromPackage(pkgPath string) string {
+	elems := strings.Split(pkgPath, "/")
+	domain := strings.Split(elems[0], ".")
+	name := ""
+	if len(elems) > 1 {
+		name = "." + elems[len(elems)-1]
+	}
+	if len(elems) < 2 && len(domain) < 2 {
+		name = "." + domain[0]
+		domain[0] = "localhost"
+	} else {
+		for i := 0; i < len(domain)/2; i++ {
+			opp := len(domain) - 1 - i
+			domain[i], domain[opp] = domain[opp], domain[i]
+		}
+	}
+
+	pkgDomain := strings.Join(domain, ".")
+	appid := []rune(pkgDomain + name)
+
+	// a Java-language-style package name may contain upper- and lower-case
+	// letters and underscores with individual parts separated by '.'.
+	// https://developer.android.com/guide/topics/manifest/manifest-element
+	for i, c := range appid {
+		if !('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' ||
+			c == '_' || c == '.') {
+			appid[i] = '_'
+		}
+	}
+	return string(appid)
 }
 
 func runCmdRaw(cmd *exec.Cmd) ([]byte, error) {
@@ -125,35 +226,6 @@ func copyFile(dst, src string) (err error) {
 	}()
 	_, err = io.Copy(w, r)
 	return err
-}
-
-type arch struct {
-	iosArch   string
-	jniArch   string
-	clangArch string
-}
-
-var allArchs = map[string]arch{
-	"arm": {
-		iosArch:   "armv7",
-		jniArch:   "armeabi-v7a",
-		clangArch: "armv7a-linux-androideabi",
-	},
-	"arm64": {
-		iosArch:   "arm64",
-		jniArch:   "arm64-v8a",
-		clangArch: "aarch64-linux-android",
-	},
-	"386": {
-		iosArch:   "i386",
-		jniArch:   "x86",
-		clangArch: "i686-linux-android",
-	},
-	"amd64": {
-		iosArch:   "x86_64",
-		jniArch:   "x86_64",
-		clangArch: "x86_64-linux-android",
-	},
 }
 
 //
@@ -199,7 +271,11 @@ func iosCompilerFor(target, arch string) (string, []string, error) {
 	return clang, cflags, nil
 }
 
-func BuildIOSFramework(tmpDir, frameworkRoot string, bi *BuildInfo) error {
+func BuildIOSFramework(tmpDir string, bi *BuildInfo) error {
+	frameworkRoot := *destPath
+	if frameworkRoot == "" {
+		frameworkRoot = fmt.Sprintf("%s.framework", strings.Title(bi.name))
+	}
 	target := bi.target
 	framework := filepath.Base(frameworkRoot)
 	const suf = ".framework"
